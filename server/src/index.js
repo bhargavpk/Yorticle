525beaa3bd9f2e030313b55a2c10650309075393
BhargavPK <bpk19900@gmail.com>
2020-10-17 03:49:33 +0530

    Fixing index.html path error

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path')
 
 require('../db/mongoose');
 
@@ -13,7 +14,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
   
     app.get('/*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+      res.sendFile(path.resolve(__dirname, '../../', 'client', 'build', 'index.html'));
     });
   }
 
